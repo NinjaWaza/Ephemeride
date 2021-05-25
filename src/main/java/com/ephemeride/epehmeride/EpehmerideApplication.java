0bae7b492d807cf17b5b6d88805cbbfe20a3d609
théodore <theodore@MacBook-Air-de-theodore.local>
2021-05-25 12:33:54 +0200

    Initialisation of the java project

---
+++ src/main/java/com/ephemeride/epehmeride/EpehmerideApplication.java
@@ -0,0 +1,13 @@
+package com.ephemeride.epehmeride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpehmerideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpehmerideApplication.class, args);
+	}
+
+}
